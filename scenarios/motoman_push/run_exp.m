0773f7c89291172e28aa06eef817c87c0715870c
zhusj <vincentzhu122@gmail.com>
2017-06-22 20:38:38 -0400

    more data

--- scenarios/motoman_push/run_exp.m
+++ scenarios/motoman_push/run_exp.m
@@ -1,11 +1,11 @@
 rd = randi(10000);
 rand('seed',rd); randn('seed',rd); 
-for i = 1:1000000
+% for i = 1:1000000
     try 
         MotomanPush_learn;
-        break;
+%         break;
     catch ME
         disp(ME);
         fprintf('REtrying...\n');
     end
-end
+% end
